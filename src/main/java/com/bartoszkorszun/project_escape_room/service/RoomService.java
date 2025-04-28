22c3222aa8edd13cad78f20e61148b734a067c2f
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 14:15:23 +0000

    added room service

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/service/RoomService.java
@@ -0,0 +1,28 @@
+package com.bartoszkorszun.project_escape_room.service;
+
+import com.bartoszkorszun.project_escape_room.model.Room;
+import com.bartoszkorszun.project_escape_room.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+
+    private final RoomRepository roomRepository;
+
+    public Room createRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    public Room getRoomById(Long id) {
+        return roomRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+    }
+}
