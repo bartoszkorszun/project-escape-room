97a648b205b7dc2e248aa70396bea57cfc6c30a9
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 11:22:25 +0000

    foxed typo

--- src/main/java/com/bartoszkorszun/project_escape_room/repository/PlayerRepository.java
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/PlayerRepository.java
@@ -4,7 +4,7 @@ import com.bartoszkorszun.project_escape_room.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Rapository
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
     Player findByUsername(String username);
 }
