6cd81336d2d02a65c4fb9727f4a0ddcbb08834b7
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 14:20:23 +0000

    added room controller

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/controller/RoomController.java
@@ -0,0 +1,32 @@
+package com.bartoszkorszun.project_escape_room.conteoller;
+
+import com.bartoszkorszun.project_escape_room.model.Room;
+import com.bartoszkorszun.project_escape_room.service.RoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+@RequiredArgsConstructor
+public class RoomController {
+
+    private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<Room> createRoom(@RequestBody Room room) {
+        return ResponseEntity.ok(roomService.createRoom(room));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Room>> getAllRooms() {
+        return ResponseEntity.ok(roomService.getAllRooms());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable Long id) {
+        return ResponseEntity.ok(roomService.getRoomById(id));
+    }
+}
