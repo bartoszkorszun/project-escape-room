065c03a43079b2485e6b54dd79c36f9e30a6fee4
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 10:17:53 +0200

    Add files via upload

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/ProjectEscapeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.bartoszkorszun.project_escape_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectEscapeRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectEscapeRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartoszkorszun/project_escape_room/ProjectEscapeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartoszkorszun.project_escape_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEscapeRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
