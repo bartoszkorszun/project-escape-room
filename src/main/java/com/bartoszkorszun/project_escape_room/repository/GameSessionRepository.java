561807e52d344f3c4639ad2bf95f9f3207d72e83
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 13:47:31 +0000

    removed autogenerated abstract methods as they were generating errors

--- src/main/java/com/bartoszkorszun/project_escape_room/repository/GameSessionRepository.java
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/GameSessionRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
-    GameSession findByGameSessionId(Long gameSessionId);
+    // TODO
 }
--- src/main/java/com/bartoszkorszun/project_escape_room/repository/PuzzleRepository.java
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/PuzzleRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PuzzleRepository extends JpaRepository<Puzzle, Long> {
-    Puzzle findByPuzzleId(Long puzzleId);
+    //TODO
 }
