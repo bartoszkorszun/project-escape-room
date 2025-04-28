415001c62d39d9241e528a7de840f205faf8442a
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 11:17:41 +0000

    created player repository

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/PlayerRepository.java
@@ -0,0 +1,10 @@
+package com.bartoszkorszun.project_escape_room.repository;
+
+import com.bartoszkorszun.project_escape_room.model.Player;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Rapository
+public interface PlayerRepository extends JpaRepository<Player, Long> {
+    Player findByUsername(String username);
+}
