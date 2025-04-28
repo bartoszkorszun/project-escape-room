c1f1a9e07f83fd1aa1cdec0cd929c3d3d88bbeba
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 11:22:14 +0000

    created puzzle repo

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/PuzzleRepository.java
@@ -0,0 +1,10 @@
+package com.bartoszkorszun.project_escape_room.repository;
+
+import com.bartoszkorszun.project_escape_room.model.Puzzle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PuzzleRepository extends JpaRepository<Puzzle, Long> {
+    Puzzle findByPuzzleId(Long puzzleId);
+}
