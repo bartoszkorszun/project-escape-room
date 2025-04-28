26c1fa38b564b6334f636f53eba09cffc5ccd0d6
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-04-28 11:19:52 +0000

    created room repo

---
+++ src/main/java/com/bartoszkorszun/project_escape_room/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.bartoszkorszun.project_escape_room.repository;
+
+import com.bartoszkorszun.project_escape_room.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+    Room findByRoomId(Long roomId);
+}
